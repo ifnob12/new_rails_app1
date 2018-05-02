54734b501838eb4f49fb2367e8d7ccedf7a6317b
ifnob12 <bogorodea.bogdancalin@yahoo.com>
2018-05-03 00:04:27 +0300

    first debug before Heroku deploy

--- app/models/concerns/food2_fork.rb
+++ app/models/concerns/food2_fork.rb
@@ -2,7 +2,7 @@ class Food2Fork
 	include HTTParty
 
 	base_uri 'http://food2fork.com/api/search'
-	default_params key: ENV["FOOD2FORK_KEY"] # , sort: r or t, page: 1 or 2 - to play with later
+	default_params key: 'ba94173fd6340140e788bfe0810a4659' # , sort: r or t, page: 1 or 2 - to play with later
 	format :json
 	
 	def self.for term
